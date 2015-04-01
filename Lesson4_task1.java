8cc58a74b3939b15a54e85d9fab2e673f4152e51
kostiantynfedorenko <kostiantynfedorenko@users.noreply.github.com>
2015-04-01 11:07:02 +0300

    Update Lesson4_task1.java

--- Lesson4_task1.java
+++ Lesson4_task1.java
@@ -12,6 +12,7 @@ public class MyTask1 {
 		// Generate massive b
 		Scanner sc = new Scanner(System.in);
 		int n;
+		int k = 0;
 		System.out.println("Please enter square massive length ' '");
 		n = sc.nextInt();
 		int[][] b = new int[n][n];
@@ -28,20 +29,16 @@ public class MyTask1 {
 		}
 		// generate reverse massive from b
 		for (int i = 0; i < b.length; i++) {
-			for (int j = 0; j < b[0].length; j++) {
-				if ((j >= b[0].length / 2) && (j < b[0].length) && (i >= 0)
-						&& (i < b.length / 2)) {
-					b[i][j] = 0;
-				} else if ((j >= 0) && (j < b[0].length / 2)
-						&& (i >= b.length / 2) && (i < b.length)) {
-					b[i][j] = 1;
-				}
+			for (int j = 0; j < b[i].length/2; j++) {
+				k = b[i][j];
+				b[i][j] =b [j][i];
+				b[j][i] = k;
+				} 
 			}
-		}
 		// print changed array b
 		System.out.println("Reverse array");
 		for (int i = 0; i < b.length; i++) {
-			for (int j = 0; j < b[0].length; j++) {
+			for (int j = 0; j < b[i].length; j++) {
 				System.out.print(b[i][j] + "\t");
 			}
 			System.out.println();
