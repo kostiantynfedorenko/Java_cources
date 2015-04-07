e50584aaa1db2d7481d15fc67f336d71b8d5e8cc
kostiantynfedorenko <kostiantynfedorenko@users.noreply.github.com>
2015-04-07 09:28:44 +0300

    Create Lesson6_MyArrayToInt.java

---
+++ Lesson6_MyArrayToInt.java
@@ -0,0 +1,36 @@
+package com.gmail.fedorenko.kostia;
+
+import java.util.Arrays;
+import java.util.Random;
+
+/**
+ * Написать свой вариант метода Arrays.toString() для int[].
+ *
+ */
+public class MyArrayToInt {
+	static StringBuffer int_to_str(int[] c, StringBuffer b) {
+		for (int i = 0; i < c.length; i++) {
+			if (i == 0) {
+				b.append(c[i]);
+			} else {
+				b.append("," + c[i]);
+			}
+		}
+		return b;
+	}
+
+	public static void main(String[] args) {
+		Random rn = new Random();
+		int[] a = new int[rn.nextInt(10)];
+		for (int i = 0; i < a.length; i++) {
+			a[i] = rn.nextInt(10);
+
+		}
+		System.out.println("Array is : " + Arrays.toString(a));
+		StringBuffer sb = new StringBuffer();
+		int_to_str(a, sb);
+		String str = sb.toString();
+		System.out.println("String is: " + str);
+	}
+
+}
